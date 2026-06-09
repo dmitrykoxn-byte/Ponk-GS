260d63fc2410df87bb65233b0e7b77133cea45f3
dmitrykoxn-byte <dmitrykoxn@gmail.com>
2026-06-09 11:28:02 +0300

    Refactor game.js to set up canvas and paddles

---
+++ script.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("game");
+const context = canvas.getContext("2d");
+
+const width = canvas.width;
+const height = canvas.height;
+
+const paddleWidth = 10;
+const paddleHeight = 100;
+const paddleOffset = 10;
+
+context.fillStyle = "#3F826D";
+context.fillRect(
+    width / 2 - paddleWidth / 2,
+    paddleOffset,
+    heigh / 2 - paddleWidth / 2
+    paddleWidth,
+    paddleHeight); 
+);
+
+context.fillStyle = "#3F826D";
+context.fillRect(
+    width / 2 - paddleWidth / 2,
+    paddleOffset,
+    height / 2 - paddleHeight / 2,
+    paddleWidth,
+    paddleHeight
+);
